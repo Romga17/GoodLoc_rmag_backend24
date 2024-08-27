70c182018025469e0fe9abe287f5cc1651c54af9
Romga17 <romgacode@gmail.com>
2024-08-27 22:14:23 +0200

    Reactivation sécurité

--- src/main/java/edu/gdlc_project/gdlc_pckgs/security/SecurityConfig.java
+++ src/main/java/edu/gdlc_project/gdlc_pckgs/security/SecurityConfig.java
@@ -1,80 +1,101 @@
-//package edu.gdlc_project.gdlc_pckgs.security;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationProvider;
-//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.CorsConfigurationSource;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//import org.springframework.web.filter.CorsFilter;
-//
-//import java.util.List;
-//
-//@EnableWebSecurity
-//@Configuration
-//@EnableMethodSecurity
-//public class SecurityConfig {
-//
-//    @Autowired
-//    AppUserDetailsService appUserDetailsService;
-//
-//    @Autowired
-//    BCryptPasswordEncoder bCryptPasswordEncoder;
-//
-//    @Autowired
-//    JwtFilter jwtFilter;
-//
-//    @Bean
-//    public AuthenticationProvider authenticationProvider() {
-//        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-//        daoAuthenticationProvider.setUserDetailsService(appUserDetailsService);
-//        daoAuthenticationProvider.setPasswordEncoder(bCryptPasswordEncoder);
-//        return daoAuthenticationProvider;
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain httpSecurity(HttpSecurity http) throws Exception {
-//        return http
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .cors(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-//                .build();
-//    }
-//
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration corsConfiguration = new CorsConfiguration();
-//
-//        // Ajoutez toutes les origines Angular qui doivent être autorisées
-//        corsConfiguration.setAllowedOrigins(List.of(
-//                "http://localhost:4200",            // Pour le développement local
-//                "http://185.97.144.183:8083"        // Adresse de production pour Angular
-//        ));
-//
-//        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "DELETE", "PUT"));
-//        corsConfiguration.setAllowedHeaders(List.of("*"));
-//        corsConfiguration.setAllowCredentials(true);
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//
-//        // Cette ligne permet à CORS de s'appliquer à toutes les routes, y compris celles avec le préfixe /api
-//        source.registerCorsConfiguration("/**", corsConfiguration);
-//
-//        return source;
-//    }
-//
-//    @Bean
-//    public CorsFilter corsFilter() {
-//        return new CorsFilter(corsConfigurationSource());
-//    }
-//}
+package edu.gdlc_project.gdlc_pckgs.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@EnableWebSecurity
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Autowired
+    AppUserDetailsService appUserDetailsService;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    JwtFilter jwtFilter;
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(appUserDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(bCryptPasswordEncoder);
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain httpSecurity(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(config -> config.configurationSource(corsConfigurationSource()))
+                .sessionManagement(config -> config.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/signin", "/register",
+                                "/booking/list", "/booking/get/{id}", "/booking/add", "/booking/validate","/booking/deny/{id}","/booking/delete/{id}", "/booking/get/valid/{id}", "/booking/get/updated",
+                                "/brand/add","/brand/list",
+                                "/category/add", "/category/list",
+                                "/design/add", "/design/list",
+                                "/document/add", "/document/list",
+                                "/incident/add/{NotifiedMaterialId}/{incidentNotifierId}","/incident/save","/incident/list","/incident/get/{id}",
+                                "/location/add","/location/list",
+                                "/material/list","/material/add","/material/get/{id}","/material/delete/{id}",
+                                "/image/add","/image/list",
+                                "/role/add","/role/list", "/role/get/{id}",
+                                "/status/add","/status/list",
+                                "/user/get/{id}","/user/list","/user/add", "/user/modify/{id}", "/user/delete/{id}",
+                                "/user/get/{id}",
+                                // Autorisation des endpoints Actuator
+                                "/actuator/**" // Autorise l'accès à tous les endpoints d'Actuator
+                        ).permitAll() // Autorise l'accès sans authentification
+                        .anyRequest().authenticated() // Toutes les autres requêtes nécessitent une authentification
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        // Ajoutez toutes les origines Angular qui doivent être autorisées
+        corsConfiguration.setAllowedOrigins(List.of(
+                "http://localhost:4200",            // Pour le développement local
+                "http://185.97.144.183:8083"        // Adresse de production pour Angular
+        ));
+
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "DELETE", "PUT"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        // Cette ligne permet à CORS de s'appliquer à toutes les routes, y compris celles avec le préfixe /api
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        return new CorsFilter(corsConfigurationSource());
+    }
+}
