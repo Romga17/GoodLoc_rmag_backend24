d538db685a7d58ce9c1561db0bd177fb11fbe3a9
Romga17 <romgacode@gmail.com>
2024-08-29 06:09:13 +0200

    ajout requestmapping api controller test

--- src/main/java/edu/gdlc_project/gdlc_pckgs/controller/AuthenticationController.java
+++ src/main/java/edu/gdlc_project/gdlc_pckgs/controller/AuthenticationController.java
@@ -14,6 +14,7 @@ import java.util.Map;
 //@CrossOrigin(origins = {"http://localhost:4200", "http://185.97.144.183:8082"})
 @RestController
 @CrossOrigin(origins = "*")
+@RequestMapping("/api")
 public class AuthenticationController {
 
     @Autowired
